18e3de961e983f7fc8b8e2188dd29350eafc3190
francisco-solis99 <franciscosoliscss@gmail.com>
2024-06-05 17:24:09 -0600

    [feat] add functions to set and get local storage item

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,19 @@
+// Functions to get data or modified
 export const sayHello = (text) => {
   return text;
+};
+
+// get the tasks from lcoal storage
+export const getTasksFromLocalStorage = ({ itemName }) => {
+  const item = window.localStorage.getItem(itemName);
+  if (!item) return { item: [] };
+
+  const itemJson = JSON.parse(item);
+  return { item: itemJson };
+};
+
+// Add a new task
+export const saveTasksForLocalStorage = ({ tasks, itemName }) => {
+  const tasksString = JSON.stringify(tasks);
+  window.localStorage.setItem(itemName, tasksString);
 };
